48985fd2a5d4bc8f37aba559285d6b414690cd17
gregs <github@gregs.io>
2023-09-27 12:50:15 +0200

    reorged code to allow more DEXs

--- contracts/Aggregator.sol
+++ contracts/Aggregator.sol
@@ -5,141 +5,54 @@ pragma solidity 0.8.18;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
 contract Aggregator {
-    IUniswapV2Router02 public immutable sRouter;
-    IUniswapV2Router02 public immutable uRouter;
-
 
     address public owner;
     uint256 defaultSlippagePercent ;
+    address[] public routers;
 
-
-    constructor(address _sRouter, address _uRouter, uint16 _defaultSlippagePercent) {
-        sRouter = IUniswapV2Router02(_sRouter); // Sushiswap
-        uRouter = IUniswapV2Router02(_uRouter); // Uniswap
+    constructor(address[] memory _routers, uint16 _defaultSlippagePercent) {
+        routers = _routers;
         owner = msg.sender;
         defaultSlippagePercent = _defaultSlippagePercent;
     }
 
-    function swapOnUniswap(
-        address[] memory _path,
-        uint256 _amountIn,
-        uint256 _amountOut
-        ) public {
-
-      _swapOnUniswapFork(
-            _path,
-            uRouter,
-            _amountIn,
-            _amountOut,
-            defaultSlippagePercent
-        );
-    }
-
-    function swapOnUniswap(
-        address[] memory _path,
-        uint256 _amountIn,
-        uint256 _amountOut,
-        uint8 _slippagePercent
-        ) public {
-
-        require(
-            _slippagePercent >= 0 && _slippagePercent <= 100,
-            'Slippage Percent should be between 0 and 100'
-        );
-
-        _swapOnUniswapFork(
-                _path,
-                uRouter,
-                _amountIn,
-                _amountOut,
-                _slippagePercent
-            );
-    }
-
-
-    function swapOnSushiswap(
-        address[] memory _path,
-        uint256 _amountIn,
-        uint256 _amountOut
-        ) public {
-
-      _swapOnUniswapFork(
-            _path,
-            sRouter,
-            _amountIn,
-            _amountOut,
-            defaultSlippagePercent
-        );
-    }
-
-    function swapOnSushiswap(
-        address[] memory _path,
-        uint256 _amountIn,
-        uint256 _amountOut,
-        uint8 _slippagePercent
-        ) public {
-
-        require(
-            _slippagePercent >= 0 && _slippagePercent <= 100,
-            'Slippage Percent should be between 0 and 100'
-        );
-
-        _swapOnUniswapFork(
-                _path,
-                uRouter,
-                _amountIn,
-                _amountOut,
-                _slippagePercent
-            );
-    }
 
     /// @notice Swap tokens on Uniswap, Sushiswap or other forks
     /// @dev _path can have several pairs
     /// @param _path An array of token addresses. path.length must be >= 2. Pools for each consecutive pair of addresses must exist and have liquidity.
-    /// @param _router address of the Uniswap router or one of its forks e.g sushiswap
-    /// @param _amountIn The amount of input tokens to send.
-    /// @param _maxAmountOut The minimum amount of output tokens that must be received for the transaction not to revert.
+    /// @param _routerAddress address of the Uniswap router or one of its forks e.g sushiswap
+    /// @param _amountIn The amount of input tokens to swap.
+    /// @param _minAmountOutBeforeSlippage The minimum amount of output tokens that must be received for the transaction not to revert.
+    /// @param _maxSlippagePercent The maximum slippage percentage allowed by the transaction
 
-    function _swapOnUniswapFork(
+    function swapOnUniswapFork(
         address[] memory _path,
-        IUniswapV2Router02 _router,
+        address _routerAddress,
         uint256 _amountIn,
-        uint256 _maxAmountOut,
-        uint256 slippagePercent
-    ) internal {
-//        console.log("_swapOnUniswapFork");
+        uint256 _minAmountOutBeforeSlippage,
+        uint256 _maxSlippagePercent
+    ) public validRouter(_routerAddress) {
 
         require (
-            IERC20(_path[0]).balanceOf(msg.sender) > _amountIn,'not enough tokens available'
-            );
-
-//       uint256 balance = IERC20(_path[0]).balanceOf(msg.sender);
-//        console.log(balance);
-//        console.log("_swapOnUniswapFork after balanceOf() call");
-
+            IERC20(_path[0]).balanceOf(msg.sender) > _amountIn,
+            'not enough tokens available'
+        );
         require(
             IERC20(_path[0]).transferFrom(msg.sender, address(this), _amountIn),
             'transferFrom failed.'
         );
-//        console.log("_swapOnUniswapFork after transferFrom() call");
-
         require(
-            IERC20(_path[0]).approve(address(_router), _amountIn),
+            IERC20(_path[0]).approve(_routerAddress, _amountIn),
             "Router approval failed."
         );
-//       console.log("_swapOnUniswapFork after approve() call");
 
-        uint256 slippage = SafeMath.div(SafeMath.mul(_maxAmountOut , slippagePercent), 100);
-        uint256 minAmountOut= SafeMath.sub(_maxAmountOut, slippage);
-
-        console.log('maxAmountOut', _maxAmountOut);
-        console.log('minAmountOut', minAmountOut);
-
-        _router.swapExactTokensForTokens(
+        uint256 slippage = SafeMath.div(SafeMath.mul(_minAmountOutBeforeSlippage , _maxSlippagePercent), 100);
+        uint256 minAmountOut= SafeMath.sub(_minAmountOutBeforeSlippage, slippage);
+        IUniswapV2Router02(_routerAddress).swapExactTokensForTokens(
             _amountIn,
             minAmountOut,
             _path,
@@ -148,37 +61,42 @@ contract Aggregator {
         );
     }
 
+
     /// @notice Find the best deal for swapping tokens on Uniswap, Sushiswap or other forks
     /// @dev _path can have several pairs,
     /// @param _path An array of token addresses. path.length must be >= 2.
-    ///
+    /// @param _amount QTY of tokens to quote exchange for
+
     function getBestAmountsOutOnUniswapForks(
         address[] memory _path,
         uint256 _amount
-    ) public view returns (uint256 bestAmount, IUniswapV2Router02 bestRouter) {
+    ) public view returns (uint256 bestAmountOut, address bestRouter){
 
-        uint256[] memory uRouterAmount = uRouter.getAmountsOut(_amount, _path);
-        uint256[] memory sRouterAmount = sRouter.getAmountsOut(_amount, _path);
-
+        uint256[] memory amountOut;
+        uint8 i = 0;
         uint lastHop = _path.length -1;
-
-        if (uRouterAmount[lastHop] >= sRouterAmount[lastHop]) {
-            bestAmount = uRouterAmount[lastHop];
-            bestRouter = uRouter;
-        } else if (uRouterAmount[lastHop] < sRouterAmount[lastHop]) {
-            bestAmount = sRouterAmount[lastHop];
-            bestRouter = sRouter;
+        for (i=0; i < routers.length; i++){
+                amountOut = IUniswapV2Router02(routers[i]).getAmountsOut(_amount, _path);
+                if (amountOut[lastHop] > bestAmountOut){
+                    bestAmountOut = amountOut[lastHop];
+                    bestRouter = routers[i];
+                }
         }
     }
 
-    function getBestPrice() public view {
-        // fetch reserve QTY of both token
-        // divide both value
-        // return result
+    modifier validRouter(address routerAddress){
+        uint8 i;
+        bool isWhiteListedRouter = false;
+        for (i=0; i < routers.length; i++){
+            if (routers[i] == routerAddress){
+                isWhiteListedRouter = true;
+                break;
+            }
+        }
+        require(
+            isWhiteListedRouter,
+            'Router must be whitelisted'
+            );
+        _;
     }
-
-    // receive() external payable {
-
-    // }
-
 }
--- test/Aggregator.js
+++ test/Aggregator.js
@@ -2,61 +2,68 @@ const { expect } = require("chai")
 const { ethers } = require('hardhat');
 
 const config = require("../config.json")
-
 const tokens = (n) => {
   return ethers.parseUnits(n.toString(), 'ether')
 }
 
 const ether = tokens
-
 const ERC20 = require("@openzeppelin/contracts/build/contracts/ERC20.json");
-
 
 describe("Aggregator", () => {
   let owner
   let aggregator
 
+  let patricio_address = "0x57757E3D981446D585Af0D9Ae4d7DF6D64647806"
+
   let USDC = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
   let WBTC = '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599'
-
   let DAI = '0x6B175474E89094C44Da98b954EedeAC495271d0F'
+  //WETH = await aggregator.WETH()
+  let WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+  path = [WETH, DAI]
+
+  let amountOut;
 
   beforeEach(async () => {
-
-    const patricio_address = "0x57757E3D981446D585Af0D9Ae4d7DF6D64647806"
 
     //  impersonating account
     await network.provider.request({
       method: "hardhat_impersonateAccount",
       params: [patricio_address],
     });
-
      investor1 = await ethers.getSigner(patricio_address);
-    //
 
+     // HH users
     [owner] = await ethers.getSigners()
 
     aggregator = await hre.ethers.deployContract(
       "Aggregator",
       [
-        config.SUSHISWAP.V2_ROUTER_02_ADDRESS,
-        config.UNISWAP.V2_ROUTER_02_ADDRESS,
+        [
+          config.UNISWAP.V2_ROUTER_02_ADDRESS,
+          config.SUSHISWAP.V2_ROUTER_02_ADDRESS,
+       //   config.SMARTDEX.V2_ROUTER_02_ADDRESS
+        ],
         1
       ]
     )
 
-    await aggregator.waitForDeployment()
-    console.log(`Aggregator contract deployed to ${await aggregator.getAddress()}`)
+    result = await aggregator.waitForDeployment()
+    //console.log(`Aggregator contract deployed to ${await aggregator.target}`)
+
+    // Approves DEX Aggregator to handle tokens
+    let weth
+    weth = new hre.ethers.Contract(WETH,ERC20.abi,investor1);
+    transaction = await weth.connect(investor1).approve(aggregator, tokens(10000))
+    result = await transaction.wait()
+    //console.log(result)
 
   })
 
   describe("Deployment", () => {
-    it("Sets the sRouter", async () => {
-      expect(await aggregator.sRouter()).to.equal(config.SUSHISWAP.V2_ROUTER_02_ADDRESS)
-    })
-
-    it("Sets the uRouter", async () => {
-      expect(await aggregator.uRouter()).to.equal(config.UNISWAP.V2_ROUTER_02_ADDRESS)
+    it("Sets the routers", async () => {
+     expect(await aggregator.routers(0)).to.equal(config.UNISWAP.V2_ROUTER_02_ADDRESS)
+     expect(await aggregator.routers(1)).to.equal(config.SUSHISWAP.V2_ROUTER_02_ADDRESS)
     })
 
     it("Sets the owner", async () => {
@@ -67,55 +74,47 @@ describe("Aggregator", () => {
  describe('Aggregates routes from several DEXs', () => {
 
     it('Returns the best route', async () => {
-        //WETH = await aggregator.WETH()
-        const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-        path = [WETH, DAI]
-
-
-        //console.log(path)
-        amountOut = await aggregator.connect(investor1)
-          .getBestAmountsOutOnUniswapForks(
-            path,
-            tokens(1000)
-          )
-       console.log('Best Swap: ', amountOut)
-
-        let weth
-        weth = new hre.ethers.Contract(WETH,ERC20.abi,investor1);
-
-        transaction = await weth.connect(investor1).approve(aggregator, tokens(10000))
-        result = await transaction.wait()
-        //console.log(result)
-
-        // transaction = await investor1.sendTransaction({from:investor1.getAddress(), to:aggregator.target, value: ether(1)});
-        // result = await transaction.wait()
-        // console.log(result)
-
-        transaction = await aggregator.connect(investor1)
-          .swapOnUniswap(
-              path,
-              tokens(1000),
-              amountOut[0],
-            )
-        result = await transaction.wait()
-        //console.log(result)
-
-        amountOut = await aggregator.connect(investor1)
+      amountOut = await aggregator.connect(investor1)
         .getBestAmountsOutOnUniswapForks(
           path,
           tokens(1000)
         )
-        console.log('Best Swap: ', amountOut)
 
-        transaction = await aggregator.connect(investor1)
-        .swapOnSushiswap(
-            path,
-            tokens(1000),
-            amountOut[0],
-          )
+      expect(amountOut[1]).to.be.properAddress;
+      expect(typeof amountOut[0]).to.equal('bigint')
+    })
+
+    it('Swaps tokens on the chosen DEX', async () => {
+      transaction = await aggregator.connect(investor1)
+      .swapOnUniswapFork(
+          path,
+          amountOut[1],  //router address
+          tokens(1000),
+          amountOut[0],
+          5
+        )
       result = await transaction.wait()
+      expect(result).to.not.be.reverted;
+    })
 
-     })
+
+    //   amountOut = await aggregator.connect(investor1)
+    //   .getBestAmountsOutOnUniswapForks(
+    //     path,
+    //     tokens(1000)
+    //   )
+    //   console.log('Best Swap: ', amountOut)
+
+    //   transaction = await aggregator.connect(investor1)
+    //   .swapOnUniswapFork(
+    //     path,
+    //     config.SUSHISWAP.V2_ROUTER_02_ADDRESS,
+    //     tokens(1000),
+    //     amountOut[0],
+    //     5
+    //   )
+    // result = await transaction.wait()
+
   })
 
 })
